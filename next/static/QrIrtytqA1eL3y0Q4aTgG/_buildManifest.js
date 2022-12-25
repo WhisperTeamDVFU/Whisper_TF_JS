847fbf50976cb6a43f08d0f25769a003328dfdf4
Maxim Skripnikov <skripnikov.mv.mail@gmail.com>
2022-12-25 23:38:29 +1000

    Add first static next js page

---
+++ next/static/QrIrtytqA1eL3y0Q4aTgG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1beb14451460885a.css","static/chunks/pages/index-0f3462ec2e26eb01.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
