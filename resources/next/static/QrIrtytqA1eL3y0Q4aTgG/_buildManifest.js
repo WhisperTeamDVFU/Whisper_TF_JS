b738b792bf13f1bb892ffd0e9e42da89723a0d21
Skripnikov M.V <hjbgbj@gmail.com>
2022-12-27 16:20:42 +1000

    Update export script

--- resources/next/static/QrIrtytqA1eL3y0Q4aTgG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1beb14451460885a.css","static/chunks/pages/index-0f3462ec2e26eb01.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
