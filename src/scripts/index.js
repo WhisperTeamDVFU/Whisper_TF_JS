00e9c1b82fa453c78575187426d420aa9ab13b0e
zalikvl25 <zalikvl25@gmail.com>
2023-02-01 20:56:06 +1000

    Add weights converter and loader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,45 @@
 import $ from "jquery";
+import { Weights } from './Weights';
 
-$(document).ready(function() {
-    $("h1").click(function() {
+$(function() {
+    $("h1").on("click", function() {
         alert("jQuery is working!");
     });
+
+    $('#model_size_select').on("change", function(){
+        alert($(this).text());
+    });
+
+    $('#input_file_weights').on('change', async function() {
+        let file = $(this)[0].files[0];
+        console.log(file);
+
+        let data = await file.arrayBuffer();
+        console.log(data);
+
+        let weights = new Weights(file.name);
+
+        await weights.init(data);
+
+        console.log(weights);
+
+        weights.get('decoder.positional_embedding').print();
+    });
+
+    $('#input_file_config').on('change', async function() {
+        let file = $(this)[0].files[0];
+        console.log(file);
+
+        var fileread = new FileReader();
+        fileread.onload = function(e) {
+        var content = e.target.result;
+
+        var intern = JSON.parse(content);
+        console.log(intern);
+        console.log(intern.n_mels);
+        };
+
+        fileread.readAsText(file);
+    });
+
 });
