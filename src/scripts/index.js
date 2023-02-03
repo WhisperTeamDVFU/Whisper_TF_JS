9ce0e144fc06f1ba847348a4e90f59eb586dc7dc
zalikvl25 <zalikvl25@gmail.com>
2023-02-03 18:04:16 +1000

    Refactor weights downlia

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,111 +2,52 @@ import $ from "jquery";
 import * as tf from "@tensorflow/tfjs";
 import { Weights } from './Weights';
 import { Whisper } from './Whisper-test';
+import tiny_url from 'url:../../tiny.hdf5';
+import base_url from 'url:../../base.hdf5';
+import CONFIGS from './Config';
+
+const MODELS_URL = {
+    "tiny": tiny_url,
+    "base": base_url,
+    "small": tiny_url,
+    "medium": tiny_url,
+    "large": tiny_url
+}
 
 var CurrentWeights;
 var CurrentCofig;
 var CurrentSizeModel = "";
-
-function exactDiv(x, y) {
-    return Math.floor(x / y);
-};
-function log10(x) {
-    let x1 = tf.log(x);
-    let x2 = tf.log(10.0);
-    return tf.div(x1, x2);
-};
-
-const SAMPLE_RATE = 16000
-const N_FFT = 400
-const N_MELS = 80
-const HOP_LENGTH = 160
-const CHUNK_LENGTH = 30
-const N_SAMPLES = CHUNK_LENGTH * SAMPLE_RATE  // 480000: number of samples in a chunk
-const N_FRAMES = exactDiv(N_SAMPLES, HOP_LENGTH)  // 3000: number of frames in a mel spectrogram input
-var audioBuffer;
-
-function audio2tensor () {
-    let file = $('#audiofile')[0].files[0];
-    let reader = new FileReader();
-
-    reader.onload = async function () {
-        const audioCtx = new (window.AudioContext || window.webkitAudioContext )();
-        const source = audioCtx.createBufferSource();
-        let audioArrBuffer = reader.result;
-        audioCtx.sampleRate = SAMPLE_RATE;
-        audioCtx.decodeAudioData(audioArrBuffer).then(async function (audioBuffer) {
-            let offlineCtx = new OfflineAudioContext(audioBuffer.numberOfChannels,
-                audioBuffer.duration * SAMPLE_RATE,
-                SAMPLE_RATE);
-            let offlineSrc = offlineCtx.createBufferSource();
-            offlineSrc.buffer = audioBuffer;
-            offlineSrc.connect(offlineCtx.destination);
-            offlineSrc.start();
-            let resampled = await offlineCtx.startRendering();
-            let audioArray = new Float32Array(resampled.length);
-            resampled.copyFromChannel(audioArray, 0, 0);
-            var tensorAudio = tf.tensor(audioArray);
-            console.log('Audio file has transformed to tensor');
-            return tensorAudio;
-        });
-    };
-    reader.readAsArrayBuffer(file);
-};
-
-$('#read_audio').on('click', logMelSpectrogram(audio2tensor));
-
-function logMelSpectrogram(audioTensor) {
-    return audioTensor
-};
 
 $(function() {
     $("h1").on("click", function() {
         alert("jQuery is working!");
     });
 
-    $('#model_size_select').on("change", function(){
-        //alert($(this).text());
+    $('#model_size_select').on("change", async function(){
         CurrentSizeModel = this.value;
         document.getElementById("CurrentSizeModel").innerHTML = CurrentSizeModel;
-    });
 
-    $('#input_file_weights').on('change', async function() {
-        let file = $(this)[0].files[0];
-        console.log(file);
-
-        let data = await file.arrayBuffer();
-        console.log(data);
-
-        let weights = new Weights(file.name);
-
+        let file_weights = await fetch(MODELS_URL[CurrentSizeModel]);
+        let data = await file_weights.arrayBuffer();
+        let weights = new Weights(CurrentSizeModel);
         await weights.init(data);
-
         CurrentWeights = weights;
         document.getElementById("CurrentWeights").innerHTML = CurrentWeights.get('decoder.positional_embedding');
-
-        console.log(weights);
-
         weights.get('decoder.positional_embedding').print();
-    });
 
-    $('#input_file_config').on('change', async function() {
-        let file = $(this)[0].files[0];
-        console.log(file);
-
-        var fileread = new FileReader();
-        fileread.onload = function(e) {
-        var content = e.target.result;
-
-        var intern = JSON.parse(content);
-
-        CurrentCofig = intern;
-        document.getElementById("CurrentCofig").innerHTML = CurrentCofig.n_mels;
-
-        console.log(intern);
-        console.log(intern.n_mels);
-        };
-
-        fileread.readAsText(file);
+        CurrentCofig = CONFIGS[CurrentSizeModel];
+        document.getElementById("CurrentCofig").innerHTML = [CurrentCofig.n_mels, 
+            CurrentCofig.n_vocab, 
+            CurrentCofig.n_audio_ctx,
+            CurrentCofig.n_audio_state,
+            CurrentCofig.n_audio_head,
+            CurrentCofig.n_audio_layer,
+            CurrentCofig.n_text_ctx,
+            CurrentCofig.n_text_state,
+            CurrentCofig.n_text_head,
+            CurrentCofig.n_text_layer,
+        ];
+        console.log(CurrentCofig);
     });
 
     $('#create_model').on('click', async function() {
